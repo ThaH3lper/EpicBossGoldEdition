2bdbe6a02db015ac5e5ff3b7b221720d28e98943
Xikage <xikage@gmail.com>
2013-10-26 22:31:42 -0700

    Added TeleportNear ability, teleports the boss to a random spot within a certain distance of the player instead of on top of them. - teleportnear horizontal_radius:vertical:radius >0 1
    Also changed it so bosses that spawn underground will dig their way out
    to the surface instead of suffocating to death or becoming stuck in
    terrain.

---
+++ src/me/ThaH3lper/com/SkillsCollection/SkillTeleportNear.java
@@ -0,0 +1,49 @@
+package me.ThaH3lper.com.SkillsCollection;
+
+import me.ThaH3lper.com.EpicBoss;
+import me.ThaH3lper.com.Skills.SkillHandler;
+
+import org.bukkit.Location;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+
+public class SkillTeleportNear {
+	
+	// teleportnear horizontal_radius:vertical_radius
+		
+	public static void ExecuteTeleportNear(LivingEntity l, String skill, Player player)
+	{
+		if(player == null) return;
+		String[] base = skill.split(" ");
+		String[] data = base[1].split(":");
+		
+		double radius_xz = Double.parseDouble(data[0]);
+		double radius_y = Double.parseDouble(data[1]);
+
+		float chance = Float.parseFloat(base[base.length-1]);
+		if(EpicBoss.r.nextFloat() < chance)
+		{
+			if(SkillHandler.CheckHealth(base[base.length-2], l, skill))
+			{	
+				Location Loc;
+				
+				Loc = player.getLocation();
+							
+				double new_x = Loc.getX();
+				double new_z = Loc.getZ();
+				double new_y = Loc.getY();
+				
+				new_x = (EpicBoss.r.nextInt(2) == 1) ? (new_x + EpicBoss.r.nextInt((int)radius_xz)) : (new_x - EpicBoss.r.nextInt((int)radius_xz));
+				new_z = (EpicBoss.r.nextInt(2) == 1) ? (new_z + EpicBoss.r.nextInt((int)radius_xz)) : (new_z - EpicBoss.r.nextInt((int)radius_xz));
+				new_y = (EpicBoss.r.nextInt(2) == 1) ? (new_y + EpicBoss.r.nextInt((int)radius_y)) : (new_y - EpicBoss.r.nextInt((int)radius_y));
+				
+				Loc.setX(new_x);
+				Loc.setZ(new_z);
+				Loc.setY(new_y);
+				
+				l.teleport(Loc);
+			}
+		}
+		
+	}
+}
